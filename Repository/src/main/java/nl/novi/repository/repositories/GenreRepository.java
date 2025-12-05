81fdd62832b5550d52dc1e01eb47fad9e18d20f2
eabr7 <e.bruins@protonmail.com>
2025-12-05 16:52:27 +0100

    Added as comment a way to use variables in application.properties.

--- Repository/src/main/java/nl/novi/repository/repositories/GenreRepository.java
+++ Repository/src/main/java/nl/novi/repository/repositories/GenreRepository.java
@@ -4,8 +4,6 @@ import nl.novi.repository.entities.GenreEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface GenreRepository extends JpaRepository<GenreEntity, Long> {
 
