26a5faea8737838f6dbe4308d69dfe164501b6e5
eabr7 <e.bruins@protonmail.com>
2025-12-05 14:54:05 +0100

    added more dummy data to data.sql

---
+++ Repository/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+
+-- Dummy data
+INSERT INTO genres (name, description) VALUES ('Rock', 'Rock muziek');
+INSERT INTO genres (name, description) VALUES ('Jazz', 'Jazz muziek');
+
+-- Dummy data
+INSERT INTO publishers (name, address, contactDetails) VALUES ('Music Brothers', 'jansenlaan 4', 'info@musicbrothers.com');
+INSERT INTO publishers (name, address, contactDetails) VALUES ('Music Studio XL', 'Pietheinstraat 1', 'info@musicstudioxl.com');
